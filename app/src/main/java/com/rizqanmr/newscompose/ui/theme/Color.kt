917bb025377bccb412e211c341218547c26d7186
Rizqanmr <45690865+Rizqanmr@users.noreply.github.com>
2024-03-19 20:52:51 +0700

    Initial commit

---
+++ app/src/main/java/com/rizqanmr/newscompose/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.rizqanmr.newscompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LightRed = Color(0xFFF75347)
+val DarkRed = Color(0xFFD3463C)
